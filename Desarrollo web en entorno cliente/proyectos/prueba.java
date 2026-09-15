edda7027d0fca693db4ff10a6ccdad19dd65ac43
jesus_zambrano_montero <ortizmonterojesus@gmail.com>
2026-09-15 21:29:11 +0200

    feat: estructura de carpetas oficial y prueba Java añadida

---
+++ Desarrollo web en entorno cliente/proyectos/prueba.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class prueba {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Introduce el primer lado: ");
+            Integer l1 = sc.nextInt();
+            System.out.println("Introduce el segundo lado: ");
+            Integer l2 = sc.nextInt();
+            System.out.println("Tu area es igual a = " + l1 * l2);
+            System.out.println("Tu perimetro es igual a = " + l1 + l2);
+        }
+    }
+}
